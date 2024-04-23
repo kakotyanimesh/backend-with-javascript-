d9900e30e846d8c023f5ca96cde5eafc1ff60c58
animesh <phb21013@tezu.ac.in>
2024-04-24 03:00:01 +0530

    pus

--- mega-APP/src/utils/asynclandelers.js
+++ mega-APP/src/utils/asynclandelers.js
@@ -1,10 +1,10 @@
-const asynchandelers = (requestHandler) => {
+const asynclandelers = (requestHandler) => {
     (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
-export {asynchandelers}
+export {asynclandelers}
 
 
 // const asynchandelers = () => {}
